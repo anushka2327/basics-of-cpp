1baa0a133fb1eef721bf96f2858c687111c3b0d0
anushka2327 <anushkashinde0703@gmail.com>
2024-07-24 11:57:27 +0530

    Add files via upload

---
+++ print hello world.cpp
@@ -0,0 +1,12 @@
+/* program to print hello world */
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<"hello world";
+    return 0;
+}
+output:
+hello world
+
+=== Code Execution Successful ===
