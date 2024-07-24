afbbd80463ab3ec73d5fa18d021c34174299b635
anushka2327 <anushkashinde0703@gmail.com>
2024-07-24 11:42:39 +0530

    Add files via upload

---
+++ size of constant.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main ()
+{
+    int c;    
+    cout<<"Enter float"<<endl;
+    cin>>c;
+    cout<<"size is :"<<c<<endl;12.356747;
+    const c;
+    c=5;
+    cout<<"Size is: "<<sizeof(c)<<endl;
+}
